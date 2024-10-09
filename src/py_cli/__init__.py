909d790f5daebbc35656421b12a38a2203ec1f9f
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-10-09 12:59:37 +0900

    feat: add click add root command
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

---
+++ src/py_cli/__init__.py
@@ -0,0 +1,36 @@
+from importlib.metadata import version
+
+import click
+
+try:
+    __version__ = version("py-cli")
+except Exception:  # noqa
+    __version__ = "0.0.0"
+
+CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
+
+
+def main() -> None:
+    cmd()
+
+
+def print_version(ctx, param, value):  # noqa
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(__version__)
+    ctx.exit()
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.option(
+    "--version",
+    "-v",
+    is_flag=True,
+    callback=print_version,
+    expose_value=False,
+    is_eager=True,
+)
+def cmd() -> None:
+    """
+    Sample Command line tool
+    """
